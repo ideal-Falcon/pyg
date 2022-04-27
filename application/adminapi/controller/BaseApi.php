a98535f6d50108413a3bbf2aac0488f9b645213b
ideal-Falcon <1760296253@qq.com>
2022-04-27 17:51:04 +0800

    BASEAPI

---
+++ application/adminapi/controller/BaseApi.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace app\adminapi\controller;
+
+use think\Controller;
+
+class BaseApi extends Controller
+{
+    public function _initialize()
+    {
+    	//处理跨域请求
+    	//允许的源域名
+	    header("Access-Control-Allow-Origin: *");
+	    //允许的请求头信息
+	    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
+	    //允许的请求类型
+	    header('Access-Control-Allow-Methods: GET, POST, PUT,DELETE,OPTIONS,PATCH');
+    }
+
+    //通用响应
+    public function response($cdoe=200,$msg='success',$data=[])
+    {
+    	$res=[
+    		'code'=>$code,
+    		'msg'=>$msg,
+    		'data'=>$data
+    	]
+    	//原生写法
+    	//echo json_decode($res,JSON_UNESCAPED_UNICODE);die;//防止转换为Unicode编码
+    	//框架写法
+    	json($res)->send();
+    }
+
+    //成功响应
+    public function ok($data=[])
+    {
+    	$this->response($data);
+    }
+    //失败响应
+    public function fail($msg,$code=500,$data=[])
+    {
+    	$this->response($code,$msg,$data);
+    }
+}
